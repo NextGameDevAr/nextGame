116ae6191c10fe95b56dccc05fec2d56dbf7d05c
TONY\iafor <TonyForns>
2023-12-12 16:01:47 -0300

    feat; clean code, remove move action from unit

---
+++ Assets/Scripts/MoveAction.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAction : MonoBehaviour
+{
+    [SerializeField] private Vector3 finalPosition;
+    [SerializeField] private Action OnReachPosition;
+    [SerializeField] private bool canMove;
+
+    public void Update()
+    {
+        if (!canMove) return;
+        if (Vector3.Distance(transform.position, finalPosition) > 0.05f)
+        {
+            transform.LookAt(finalPosition);
+            Vector3 moveDirection = (finalPosition - transform.position).normalized;
+            float moveSpeed = 4;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+        }
+        else
+        {
+            canMove = false;
+            OnReachPosition?.Invoke();
+            transform.position = finalPosition;
+        }
+
+    }
+    public void Move(Vector3 finalPosition, Action onReachPosition)
+    {
+        canMove = true;
+        OnReachPosition = onReachPosition;
+        this.finalPosition = finalPosition;
+    }
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,40 +6,19 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] internal MoveAction moveAction;
+    [SerializeField] internal Transform itemPosition;
 
-    [SerializeField] private Vector3 finalPosition;
-    [SerializeField] private Action OnReachPosition;
-    [SerializeField] private Item currentItem;
-    [SerializeField] private Transform itemPosition;
+    private void Awake()
+    {
+        moveAction = GetComponent<MoveAction>();
+    }
 
     public void Update()
     {
-        if (finalPosition == transform.position) return;
-
-        if(Vector3.Distance(transform.position, finalPosition) > 0.05f)
-        {
-            transform.LookAt(finalPosition);
-            Vector3 moveDirection = (finalPosition - transform.position).normalized;
-            float moveSpeed = 4;
-            transform.position += moveDirection * Time.deltaTime * moveSpeed;
-
-        } else
-        {
-            OnReachPosition?.Invoke();
-            transform.position = finalPosition;
-        }
-
     }
-
-    public void SetItem(Item item)
-    {
-        currentItem = item;
-        item.SetParent(itemPosition);
-    }
-
     public void Move(Vector3 finalPosition, Action onReachPosition)
     {
-        OnReachPosition = onReachPosition;
-        this.finalPosition = finalPosition;
+       moveAction.Move(finalPosition, onReachPosition);
     }
 }
