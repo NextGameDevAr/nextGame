a81402f9af9702a23781a4a32a3f0ce3209961c0
TONY\iafor <TonyForns>
2023-12-12 16:03:55 -0300

    feat; create material and prefab for scene and ser it into the game

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,31 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    [SerializeField] private Unit unit;
-    [SerializeField] private Station currentStation;
+    [SerializeField] OrderCounter counter;
+   
     void Start()
     {
     }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            if (MouseWorld.TryToGetStation(out currentStation)) {
-                if (currentStation.IsWorking())
-                {
-                    currentStation.IncreaseProgress();
-                } else
-                {
-                    unit.Move(currentStation.GetFrontPosition(), () => currentStation.StartInteraction(OnFInish)); ;
-                }
-            }
-        }
+       
     }
 
-    private void OnFInish(Item item)
-    {
-        currentStation = null;
-        unit.SetItem(item);
-    }
+    
 }
