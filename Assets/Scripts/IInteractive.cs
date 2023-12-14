7f8c24f08209fb47a4416ac085f0744a206462c1
TONY\iafor <TonyForns>
2023-12-14 18:28:27 -0300

    feat: Update prefabs and add exit script. Add interactive interface script

---
+++ Assets/Scripts/IInteractive.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractive 
+{
+    public Vector3 GetUnitSpotPosition();
+    public void Interact(Action OnFinishInteraction);
+}
